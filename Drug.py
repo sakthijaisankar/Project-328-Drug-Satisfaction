7817fbc148278ae8c0255c131c35c74c2a2772b8
Sakthi J <146372939+sakthijaisankar@users.noreply.github.com>
2024-02-20 11:45:10 +0530

    Drug.py

--- Drug.py
+++ Drug.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
